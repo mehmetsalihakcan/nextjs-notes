edec2bf30ee4d0347fcc71b8b1df44edfc23a7ea
mehmetsalihakcan <akcanmsa@gmail.com>
2024-07-23 23:08:13 +0300

    static page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// http://localhost:3000/dashboard  statik bir sayfadır
+function Dashboard() {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
